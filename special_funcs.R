f34461c45c50fb98a485309b409e17cd48dd58a1
Carr <pcarr@gssla17011792.ndc.nasa.gov>
2017-06-30 16:39:24 -0400

    fixed the math in node.rate

--- special_funcs.R
+++ special_funcs.R
@@ -5,3 +5,12 @@ sma.from.period <- function(period,mu) {
 	a <- (mu/n^2)^(1/3)
 	return(a)
 }
+
+#####################################
+# returns rate of nodal precession for J2, inclination, semi-major axis, radius of primary,eccentricity, and mu
+# returned units wil be consistent with mu. Inclination should be in radians
+node.rate <- function(J2,sma,Rstar,incl_rad,ecc,mu) {
+	semip = sma/Rstar*(1-ecc^2)	# semiparameter
+	mean.motion = sqrt(mu/sma^3)
+	return(-1.5*J2/semip^2*cos(incl_rad)*mean.motion)
+}
