5909a761e880e7f6f521e63ddab8e686fda740cc
Carr <pcarr@gssla17011792.ndc.nasa.gov>
2017-06-30 16:03:41 -0400

    fixed a syntax error

--- special_funcs.R
+++ special_funcs.R
@@ -2,5 +2,6 @@
 # returns Keplerian semi-major axis in units consistent with period and mu
 sma.from.period <- function(period,mu) {
 	n <- 2*pi/period		# mean motion
-	return (mu/n^2)^(1/3)
+	a <- (mu/n^2)^(1/3)
+	return(a)
 }
