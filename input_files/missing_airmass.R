02695bf52c3e85e720123f3e7a182041ede75ad2
Paul Carr <PaulCarr@Pauls-Air.home>
2017-07-20 23:00:14 -0400

    new observations, new observer

--- input_files/missing_airmass.R
+++ input_files/missing_airmass.R
@@ -6,6 +6,7 @@ Erd <- c(47.39,18.90454) # approx. location of MATA
 Haan <- c(51.192,7.007) # approx. location of WROC
 LosAngeles <- c(34.05,-118.25) # approx. location of FJQ
 Potomac <- c(38.02,-77.21) # approx location of PALE
+Coventry <- c(52.4,-1.52) # approximate location of KTHC
 tabbysLoc <- c("+44d 27m 24.61s","20h 06m 15.457s") # right ascension and declination of the star.
-missingAirmass <- c("JM","PXR","JSJA","MATA","WROC","FJQ","PALE")	# observer code
-missingAMLocs = rbind(lasCruces,Leominster,Warrington,Erd,Haan,LosAngeles,Potomac)
+missingAirmass <- c("JM","PXR","JSJA","MATA","WROC","FJQ","PALE","KTHC")	# observer code
+missingAMLocs = rbind(lasCruces,Leominster,Warrington,Erd,Haan,LosAngeles,Potomac,Coventry)
