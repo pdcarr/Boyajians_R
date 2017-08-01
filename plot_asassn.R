58b40db630486c7f43a1a645c4f7363731525285
Paul Carr <PaulCarr@MacBook.home>
2017-07-31 23:02:14 -0400

    update ASASSN plotting with latest data

--- plot_asassn.R
+++ plot_asassn.R
@@ -7,14 +7,14 @@ library("crayon") # to add color to text
 source("plot_funcs.R")
 
 # the name of the file
-asassn.csv.file <- "data/light_curve_ab426ded-039a-4c44-8976-ef7fa220899f.csv"
+asassn.csv.file <- "data/ASASSN_latest.csv"
 # cull limits to reject really wild points
 mag.cull.limit = 0.05
 # mag margin for expanding plot limits (positive number)
 mag.margin <- 0.025
 ####### MARS
-marsOrder <- 21 # maximum number of knots
-marsPenalty <- 3 # set to 0 to avoid penalizing knots in pruning pass
+marsOrder <- 11 # maximum number of knots
+marsPenalty <- 4 # set to 0 to avoid penalizing knots in pruning pass
 #marsPMethod <- "none" # set to "none" to avoid pruning
 marsPMethod <- "backward" # set to "none" to avoid pruning
 
