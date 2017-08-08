ea8dc88bf7cd06a9da8595ea95101be7a7057a9f
Paul Carr <paul.david.carr@gmail.com>
2017-08-07 21:10:42 -0400

    asassn plot tweaks

--- plot_asassn.R
+++ plot_asassn.R
@@ -42,9 +42,10 @@ my.xlab <- paste("Julian Date - ",tmin)
 my.x.lims <- c(0,max(desmat) + tmargin)
 # plot
 plot(desmat,asassn_data$mag[good_data],col="darkgreen",pch=20,main="ASASSN V Data",xlab=my.xlab,ylab="V magnitude",ylim=my.y.lims,xlim= my.x.lims)
-
+# add grid
 grid(col="black")
-#plot MARS fir as a line
+
+# plot MARS fir as a line
 lines(x=desmat,y=mars$fitted.values,col= "black",lwd=2)
 # plot vertical lines if any
 jdLine <- jdLine - tmin
