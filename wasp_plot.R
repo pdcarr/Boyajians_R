5060277cf03fe219c1180fe04f70dfd705875eeb
Paul Carr <paul.david.carr@gmail.com>
2017-08-17 15:23:45 -0400

    updated file path for WASP data

--- wasp_plot.R
+++ wasp_plot.R
@@ -1,7 +1,7 @@
 # read in, fit and plot the wasp data
 library(MASS)
 
-wasp_data <- read.csv("wasp.csv",header=FALSE)
+wasp_data <- read.csv("data/wasp.csv",header=FALSE)
 
 plot(wasp_data$V2,wasp_data$V3,col="red",pch=20,cex=0.7,ylim=c(max(wasp_data$V3,na.rm=TRUE),min(wasp_data$V3,na.rm=TRUE)),xlab="Julian Date",ylab="Magnitude",main="WASP Data")
 
