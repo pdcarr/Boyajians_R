0e13c5e4e3fef858f5e4db9bd23de8fef4eec647
Paul Carr <paul.david.carr@gmail.com>
2017-08-25 14:27:02 -0400

    added ability plot subset of periodogram

--- BG_LSP.R
+++ BG_LSP.R
@@ -2,6 +2,8 @@ library(lomb)
 # lightcurve has been read in by the aavso plottng script with names autoassigned by read.csv()
 # cleanBand is output fron the plotting scrpts cleaning process: airmass, comparison stars, etc.
 fit.resids <- TRUE
+limit.period <- TRUE
+max.period <- 40 # days
 set.ofac <- 8
 use.frequency <- FALSE # set this to FALSE to plot vs. period
 desmat <- bg.data$MJD[ok.airmass] - tmin
@@ -26,8 +28,12 @@ if(use.frequency) {
 	plot(mylsp$scanned,mylsp$power,xlab="Frequency (cycles/day)",ylab="normalized power",col="darkgreen",type="l",main= title.string)
 } else {
 	mylsp <- lsp(x=theFit$residuals,times=mytimes,type="period",ofac=set.ofac,alpha=0.01)
-	plot(mylsp$scanned,mylsp$power,xlab="Period (days)",ylab="power",col="darkgreen",type="l",main=title.string)
-
+	if(limit.period) {
+		myxlims <- c(0,max.period)
+		plot(mylsp$scanned,mylsp$power,xlab="Period (days)",ylab="power",col="darkgreen",type="l",main=title.string,xlim=myxlims)
+	} else {
+		plot(mylsp$scanned,mylsp$power,xlab="Period (days)",ylab="power",col="darkgreen",type="l",main=title.string)
+	}
 }
 
 grid(col="black")
