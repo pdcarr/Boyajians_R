c192e4096c8903a2ef3eb02c92510007c09b862d
Paul Carr <paul.david.carr@gmail.com>
2017-09-05 12:49:24 -0400

    tweaks to LSP

--- BG_LSP.R
+++ BG_LSP.R
@@ -1,4 +1,6 @@
 library(lomb)
+library("MASS") # for rlm() and lqs()
+
 # lightcurve has been read in by the aavso plottng script with names autoassigned by read.csv()
 # cleanBand is output fron the plotting scrpts cleaning process: airmass, comparison stars, etc.
 fit.resids <- TRUE
@@ -6,28 +8,37 @@ limit.period <- TRUE
 max.period <- 40 # days
 set.ofac <- 8
 use.frequency <- FALSE # set this to FALSE to plot vs. period
-desmat <- bg.data$MJD[ok.airmass] - tmin
-theFit <- rlm(bg.data$V.mag[ok.airmass] ~ desmat,na.action="na.omit",psi=psi.bisquare)
 
-mytimes <- desmat
 quartz("LSP of Bruce Gary data")
 title.string <- "Bruce Gary Data L-S Periodogram"
 
 # confined Gaussian window
-w <- cg.window(mytimes)
+if (bin.it) {
+		desmat <- allSuperObs$MJD - tmin
+		theFit <- rlm(allSuperObs$V.mag ~ desmat,na.action="na.omit",psi=psi.bisquare)
+		mytimes <- desmat
+		w <- cg.window(mytimes)
+		X <- allSuperObs$V.mag
+	} else {
+		desmat <- bg.data$MJD[ok.airmass] - tmin
+		theFit <- rlm(bg.data$V.mag[ok.airmass] ~ desmat,na.action="na.omit",psi=psi.bisquare)
+		mytimes <- desmat
+		w <- vector(n= length(mytimes),mode="numeric")
+		w < 1	# rectangular window
+	}
 	
 if (fit.resids) {
 	X <- as.vector(theFit$residuals*t(w))
 } else {
-	X <- as.vector(bg.data$V.mag[ok.airmass]*t(w))
+	X <- as.vector(X*t(w))
 }
 
 
 if(use.frequency) {
-	mylsp <- lsp(x=theFit$residuals,times=mytimes,from=0.1,to=3.5,type="frequency",ofac=set.ofac,alpha=0.01)
+	mylsp <- lsp(x=X,times=mytimes,from=0.1,to=3.5,type="frequency",ofac=set.ofac,alpha=0.01)
 	plot(mylsp$scanned,mylsp$power,xlab="Frequency (cycles/day)",ylab="normalized power",col="darkgreen",type="l",main= title.string)
 } else {
-	mylsp <- lsp(x=theFit$residuals,times=mytimes,type="period",ofac=set.ofac,alpha=0.01)
+	mylsp <- lsp(x=X,times=mytimes,type="period",ofac=set.ofac,alpha=0.01)
 	if(limit.period) {
 		myxlims <- c(0,max.period)
 		plot(mylsp$scanned,mylsp$power,xlab="Period (days)",ylab="power",col="darkgreen",type="l",main=title.string,xlim=myxlims)
