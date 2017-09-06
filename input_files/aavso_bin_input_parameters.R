ecdd529fc57dfc6f21f73a0caab96269321e9ae4
Paul Carr <paul.david.carr@gmail.com>
2017-09-06 11:09:58 -0400

    added residual plot type

--- input_files/aavso_bin_input_parameters.R
+++ input_files/aavso_bin_input_parameters.R
@@ -1,7 +1,7 @@
 ##################
 # input parameters for binning and plotting AAVSO data
 llightcurve_name <- "data/aavso_latest_data.csv"
-maxairmass <- 2.0 # air mass values above this will be filtered out, as well as missing air masses. Set >= 100 to turn this off
+maxairmass <- 1.75 # air mass values above this will be filtered out, as well as missing air masses. Set >= 100 to turn this off
 maxuncertainty <- 0.1  # maximum AAVSO uncertainty estimate
 maxBinUncertainty <- 0.1 # worst standard deviation to accept for a binned set of observations
 wildsd <- 6.0 # worst number of standard deviations from mean allowed
@@ -17,22 +17,21 @@ includeExclude <- TRUE # TRUE if your list of observer codes is to to be include
 ExclCodes <- "None"
 #ExclCodes <- c("JM","LDJ","ELYA","DKS","OJJ","OAR","ATE","BPAD","HJW")
 #ExclCodes <- c("JM","LDJ","DUBF","HJW","PXR","DKS","OJJ","HBB","SDB","VBPA","OAS","MJB","MATA","JSJA","UJHA","WROC","MAND","HDHA","ELYA","VBPA","NOT","PALE") # V & B
-ExclCodes <- c("LDJ","DUBF","HJW","PXR","DKS","OJJ","HBB","SDB","VBPA","OAS","MJB","MATA","JSJA","WROC","MAND","HDHA","ELYA","VBPA","NOT","PALE") # V & B
-#ExclCodes <- c("DUBF","DKS","ELYA","OAR","ATE","HJW","BPAD","OJJ","LBG","LDJ","UJHA","OYE","GFRB","OAS","MJB","EEY") # V ensemble
+ExclCodes <- c("LDJ","DUBF","HJW","PXR","DKS","OJJ","HBB","SDB","VBPA","OAS","MJB","MATA","JSJA","WROC","MAND","HDHA","ELYA","VBPA","NOT","PALE","LPAC") # V & B
 #ExclCodes <- c("DUBF","GKA","BPAD","LPB","SJAR","LBG","LDJ","LWHA") # R ensemble
 #ExclCodes <- c("OAR","OJJ","GKA","MJB","SJAR","LWHA","LBG","LPB","LDJ","CMP") # I ensemble
 #ExclCodes <- c("DUBF","MJB","LDJ","GKA","ELYA","HJW","DUBF","JSJA","VBPA","DKS") # candidate B ensemble
-#ExclCodes <- c("LDJ")
+ExclCodes <- c("LDJ")
 #ExclCodes <- c("SWIA","CPP")	# B band exclusions
 #ExclCodes <- c("BJFB","COO","LBG","BMAK") # I band exclusion
 #ExclCodes <- c("LDJ","DUBF","UJHA","DKS","HBB")
 #ExclCodes <- "JM"
 ########
 plotMee <- NA # do not highlight any particular observer code
-#plotMee <- "ELYA"
+plotMee <- "RZD"
 meeColor <- "darkviolet"
 ########
-#allBands <- data.frame(bandinQ=c("I","R","V","B"),plotColor=c("darkviolet","red","green","blue"), stringsAsFactors=FALSE)
+allBands <- data.frame(bandinQ=c("I","R","V","B"),plotColor=c("darkviolet","red","green","blue"), stringsAsFactors=FALSE)
 allBands <- data.frame(bandinQ=c("V"),plotColor=c("darkgreen"), stringsAsFactors=FALSE)
 #allBands <- data.frame(bandinQ=c("V","B"),plotColor=c("green","blue"), stringsAsFactors=FALSE)
 #allBands <- data.frame(bandinQ=c("B"),plotColor=c("blue"), stringsAsFactors=FALSE)
@@ -47,19 +46,20 @@ deltaJD <- 1.0 # bin width in days
 plotExcluded <- FALSE # set to TRUE to plot the points in the lightcurve not used in the fit.
 plotQuadratic <- FALSE # set to TRUE to plot a quadratic fit
 generateTS <- TRUE # set to TRUE to creat a time series from the data
-tsBinWidth <- 8.0 # time series bin width in days. Important if generateTS is TRUE
+tsBinWidth <- 2.0 # time series bin width in days. Important if generateTS is TRUE
 smoothTS <- TRUE # set to TRUE to smooth the times series
-tsSmoothOrder <- 10 # the order for the moving average to smooth the time series
+tsSmoothOrder <- 6 # the order for the moving average to smooth the time series
 tryLQS <- FALSE # set to TRUE is you want to try resistant regression.
 userlm <- TRUE # set to TRUE to use robust lm, or rlm() if not using MARS.
 plotMARS <-  TRUE # set to TRUE to try a MARS fit instead of lm() or rlm()
 plotResiduals <- TRUE # set to true to plot the residuals vs. time
+res.plot.type = "l"
 plot2Lines <-  FALSE  # two line feature doesn't work well
 lqsColor <- "darkgreen"
 weightedBins <- FALSE # set to TRUE to weight lower uncertainty bins more.
 
 ####### MARS
-marsOrder <- 11 # maximum number of knots
+marsOrder <- 21 # maximum number of knots
 marsPenalty <- 4 # set to 0 to avoid penalizing knots in pruning pass
 #marsPMethod <- "none" # set to "none" to avoid pruning
 marsPMethod <- "backward" # set to "none" to avoid pruning
