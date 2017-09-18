f709c1060adf844ac00c78ff81d6b1217493aece
Paul Carr <paul.david.carr@gmail.com>
2017-09-18 09:32:10 -0400

    updated dip name to Angkora in mask

--- input_files/dip_mask.R
+++ input_files/dip_mask.R
@@ -1,4 +1,4 @@
 dip.mask <- data.frame(dipname="Elsie",JD.begins = 2457890,JD.ends=2457900,stringsAsFactors=FALSE)
 dip.mask <- rbind(dip.mask,c("Celeste",2457910,2457930),
 						   c("Skara Brae",2457965,2457990),
-						   c("Nameworthy",2458002,2458010))
+						   c("Angkora",2458002,2458010))
