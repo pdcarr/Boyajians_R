1b3600455d9282d73d0451670f46aa672db284e6
Paul Carr <paul.david.carr@gmail.com>
2017-09-18 09:27:36 -0400

    added some more useful facts

--- input_files/facts_MKS.R
+++ input_files/facts_MKS.R
@@ -3,8 +3,11 @@ R.Earth <- 6378e3 # radius of the Earth in meters
 R.Jupiter <- 69.911e6 # radius of Jupiter
 R.star <- 1.1e9	# radius if the star in meters
 M.star <- 2.84e30 # mass of the star in kilograms
+M.Jupiter <- 1.898e27 # mass of Jupiter in kg
+M.Earth <- 5.972e24 # mass of Earth in kg
 L.star <- 1.8e28 # luminosity of the star in Watts
 G.const <- 6.674e-11 # gravitational constant Newton meters^2/kg^2
 Pl.const <- 6.62607e-34 # Planck's constant in Joule seconds
 mu.star <- M.star*G.const
 omega.star = 2*pi/(0.88*86400) # angular rate of the star's outer atmosphere in radians per second
+seconds.ms.day <- 86400 #seconds in a mean solar day
