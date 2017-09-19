2d87b14bb2eb3c57b5f6d8a85eb4f21f7c381edd
Paul Carr <paul.david.carr@gmail.com>
2017-09-18 23:16:31 -0400

    tweaking dip start times

--- input_files/dip_mask.R
+++ input_files/dip_mask.R
@@ -1,4 +1,4 @@
-dip.mask <- data.frame(dipname="Elsie",JD.begins = 2457890,JD.ends=2457900,stringsAsFactors=FALSE)
-dip.mask <- rbind(dip.mask,c("Celeste",2457910,2457930),
-						   c("Skara Brae",2457965,2457990),
+dip.mask <- data.frame(dipname="Elsie",JD.begins = 2457890,JD.ends=2457896,stringsAsFactors=FALSE)
+dip.mask <- rbind(dip.mask,c("Celeste",2457915,2457927),
+						   c("Skara Brae",2457968,2457980),
 						   c("Angkor",2458002,2458010))
