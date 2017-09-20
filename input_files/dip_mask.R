27bd2beb129efe792824b2db48c30adf0812ca09
Paul Carr <paul.david.carr@gmail.com>
2017-09-20 09:41:07 -0400

    extended Skara Bra a bit past Simon paper

--- input_files/dip_mask.R
+++ input_files/dip_mask.R
@@ -1,4 +1,4 @@
 dip.mask <- data.frame(dipname="Elsie",JD.begins = 2457890,JD.ends=2457896,stringsAsFactors=FALSE)
 dip.mask <- rbind(dip.mask,c("Celeste",2457915,2457927),
-						   c("Skara Brae",2457968,2457980),
+						   c("Skara Brae",2457968,2457985),
 						   c("Angkor",2458002,2458010))
