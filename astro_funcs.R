92778adde29f4cb7e2cb96a909ecfcda94293181
Paul Carr <paul.david.carr@gmail.com>
2017-10-03 13:49:33 -0700

    debugged AirMass function

--- astro_funcs.R
+++ astro_funcs.R
@@ -13,14 +13,18 @@ AirMass <- function(JD,locObs,starLoc) {
 	# calculate UT times
 	utStrings <- dmjd2ut(mJD,tz="UTC")
 	
+#	print(locObs)
+#	print(class(locObs[1]))
+	
 	#break out the elements of the time as a POSIXlt class object
 	ltTimes <- as.POSIXlt(utStrings)
 	# calculate hour angles at the observatory
-	myHAs <- ut2ha(yr=ltTimes$year,mo=ltTimes$mon + 1,dy=ltTimes$mday,hr=ltTimes$hour,mi=ltTimes$min,se=ltTimes$sec,ra.sou =starLoc[2],lon.obs=rad2hms(locObs[2]*pi/180))
+	myHAs <- ut2ha(yr=ltTimes$year +1900,mo=ltTimes$mon + 1,dy=ltTimes$mday,hr=ltTimes$hour,mi=ltTimes$min,se=ltTimes$sec,
+					ra.sou =starLoc[2],lon.obs=rad2hms(locObs[2]*pi/180))
 #	print(myHAs)
 	#calculate elevation angles from hour angles and observatory latitude
 	myEls = elev(dec.sou=starLoc[1],ha=myHAs,lat.obs=rad2dms(locObs[1]*pi/180))
-#	print(myEls)
+	print(myEls)
 	return(1/abs(sin(myEls*pi/180)))
 }
 
@@ -31,7 +35,7 @@ ReverseMagnitude <- function(relMag) {
 }
 
 ##########################################################
-
+# returns approximate Hill radius in units consistent with a for mass of secondary (m) and Primary (M)
 HillRadius <- function(m=1,M=1000,a,e=0) {
 	return(a*(1-e)*(m/(3*M))^(1/3))
 }
