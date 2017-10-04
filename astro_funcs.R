61f3da6be84a63b9880a48edaed9b5891adfce35
Paul Carr <paul.david.carr@gmail.com>
2017-10-04 11:18:55 -0700

    fixed a bug in Airmass

--- astro_funcs.R
+++ astro_funcs.R
@@ -6,22 +6,21 @@ AirMass <- function(JD,locObs,starLoc) {
 # calculates Airmass from the time, the observer's location, and the declination of the star
 # uses astroFns package
 	library(astroFns)
-	
+#	print(locObs)
 	# modified Julian Date
 	mJD <- JD - 2400000.5
 	
 	# calculate UT times
 	utStrings <- dmjd2ut(mJD,tz="UTC")
-	
-#	print(locObs)
-#	print(class(locObs[1]))
+#	print(tail(utStrings))
 	
 	#break out the elements of the time as a POSIXlt class object
 	ltTimes <- as.POSIXlt(utStrings)
 	# calculate hour angles at the observatory
 	myHAs <- ut2ha(yr=ltTimes$year +1900,mo=ltTimes$mon + 1,dy=ltTimes$mday,hr=ltTimes$hour,mi=ltTimes$min,se=ltTimes$sec,
-					ra.sou =starLoc[2],lon.obs=rad2hms(locObs[2]*pi/180))
-#	print(myHAs)
+					ra.sou =starLoc[2],lon.obs=rad2dms(locObs[2]*pi/180))
+# 	print(tail(ltTimes))
+#	print(tail(myHAs))
 	#calculate elevation angles from hour angles and observatory latitude
 	myEls = elev(dec.sou=starLoc[1],ha=myHAs,lat.obs=rad2dms(locObs[1]*pi/180))
 #	print(myEls)
