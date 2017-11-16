ccaa5025bc906e5cc75cddf7e2d8e0b0eb28034e
Paul Carr <paul.david.carr@gmail.com>
2017-11-16 15:09:49 -0500

    updated to spline fit to calculate residuals

--- BG_LSP.R
+++ BG_LSP.R
@@ -1,39 +1,65 @@
 library(lomb)
 library("MASS") # for rlm() and lqs()
+source("special_funcs.R")
 
 # lightcurve has been read in by the aavso plottng script with names autoassigned by read.csv()
-# cleanBand is output fron the plotting scrpts cleaning process: airmass, comparison stars, etc.
+# cleanBand is output fron the plotting script's cleaning process
 fit.resids <- TRUE
 limit.period <- TRUE
-max.period <- 40 # days
+max.period <- 20 # days
 set.ofac <- 8
 use.frequency <- FALSE # set this to FALSE to plot vs. period
+######## fitting parameters
+bg.n.knots <- 5
 
 quartz("LSP of Bruce Gary data")
 title.string <- "Bruce Gary Data L-S Periodogram"
 
 # confined Gaussian window
 if (bin.it) {
-		desmat <- allSuperObs$MJD - tmin
-		theFit <- rlm(allSuperObs$V.mag ~ desmat,na.action="na.omit",psi=psi.bisquare)
-		mytimes <- desmat
-		w <- cg.window(mytimes)
-		X <- allSuperObs$V.mag
-	} else {
-		desmat <- bg.data$MJD[ok.airmass] - tmin
-		theFit <- rlm(bg.data$V.mag[ok.airmass] ~ desmat,na.action="na.omit",psi=psi.bisquare)
-		mytimes <- desmat
-		w <- vector(n= length(mytimes),mode="numeric")
-		w < 1	# rectangular window
-	}
+	desmat <- allSuperObs$MJD - tmin
+	bg.binned.JDs <- allSuperObs$MJD  + 2400000.5
+    dipless <- filter.dips.JD(bg.binned.JDs,dip.mask)
+    binWeights <- as.numeric(dipless) # weight of 1 if not in a known dip or flare, 0 otherwise.
+	theFit <- smooth.spline(x=desmat,
+					y= allSuperObs$V.mag,
+					w= binWeights,
+					all.knots=FALSE,nknots= bg.n.knots,
+					keep.data=TRUE,
+					cv=TRUE,
+					penalty= 1)
+					
+	cat("\n\n Smooth Spline Fit: \n")
+	print(theFit $call)
+	cat("\n")
+	print(theFit $fit)
+	fit.points <- predict(theFit,x=desmat)$y
+	mytimes <- desmat
+	w <- cg.window(mytimes)
+	X <- allSuperObs$V.mag
+} else {
+	desmat <- bg.data$MJD[ok.airmass] - tmin
+	theFit <- rlm(bg.data$V.mag[ok.airmass] ~ desmat,na.action="na.omit",psi=psi.bisquare)
+	mytimes <- desmat
+	w <- vector(n= length(mytimes),mode="numeric")
+	w <- 1	# rectangular window
+}
 	
 if (fit.resids) {
-	X <- as.vector(theFit$residuals*t(w))
+	X <- (allSuperObs$V.mag - fit.points)
+	quartz("residuals plot")
+	plot(x= mytimes[dipless],y=X[dipless],col='red', 
+		pch=20,
+		xlab="time",ylab="residual")
+	points(x= mytimes[!dipless],y=X[!dipless],col='grey',pch=20)
+	lines(x=mytimes,y=fit.points,col="darkgrey",lwd=2)
+	grid(col="black")
+	X <- as.vector(X*t(w))
 } else {
 	X <- as.vector(X*t(w))
 }
 
-
+quartz("Periodogram")
 if(use.frequency) {
 	mylsp <- lsp(x=X,times=mytimes,from=0.1,to=3.5,type="frequency",ofac=set.ofac,alpha=0.01)
 	plot(mylsp$scanned,mylsp$power,xlab="Frequency (cycles/day)",ylab="normalized power",col="darkgreen",type="l",main= title.string)
