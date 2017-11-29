b2b935759f96fb6c777ca20a888b948d7ec9d1c3
Paul Carr <paul.david.carr@gmail.com>
2017-11-29 15:40:15 -0500

    many improvements to NEOWISE plotting

--- NEOWISE_2017.R
+++ NEOWISE_2017.R
@@ -1,23 +1,30 @@
+library("MASS") # for rlm() and lqs()
+library("Hmisc")
+
 ########################### Parameters
 #name of file
 NW.file <- "data/NEOWISE_2017DR.csv"
 # bin width in days
-bin.width = 2 # days
+bin.width = 1 # days
 plot.w1 = FALSE
 w1.col = "blue"
 w2.col="darkgreen"
 bar.col="lightgrey"
 pretty.mag <- 100
 y.marg <- 0.02
+worst.qual <- 10
 ########################### Process
 #read the .csv file
 neowise.data <- read.csv(file=NW.file,stringsAsFactors=FALSE)
-#clean the data
+
+#clean the data for contamination and confusion flags and low quality. flagged points won't be included in bins
 ok.cc <- regexpr(pattern="^00",text= neowise.data$cc_flags,perl=TRUE) ==  1
+ok.cc <- ok.cc & (neowise.data$qual_frame >= worst.qual)
+
 # bin the w1 and w2 data
 
 tmin = floor(min(neowise.data$mjd,na.rm=TRUE)) #floor time (modified Julian date)
-tmax = ceil(max(neowise.data$mjd,na.rm=TRUE))	#ceiling time
+tmax = ceiling(max(neowise.data$mjd,na.rm=TRUE))	#ceiling time
 
 # create data frames
 allSuperObs <- data.frame(mjd=numeric(),w1=numeric(),w1.sigma=numeric(),w2=numeric(),w2.sigma=numeric(),stringsAsFactors=FALSE)
@@ -40,23 +47,51 @@ for (t in seq(tmin,tmax,by=bin.width)) {
 }
 
 # calculate x and y limits
-y.limits <- c(ceil(max(allSuperObs$w2,na.rm=TRUE)*pretty.mag)/pretty.mag + y.marg,
-				floor(min(allSuperObs$w2,na.rm=TRUE)*pretty.mag)/pretty.mag - y.marg)
+y.limits <- c(ceiling(max(c(allSuperObs$w1,allSuperObs$w2),na.rm=TRUE)*pretty.mag)/pretty.mag + y.marg,
+				floor(min(c(allSuperObs$w1,allSuperObs$w2),na.rm=TRUE)*pretty.mag)/pretty.mag - y.marg)
 t.limits <- c(tmin,tmax)
 # plot the bins
 
 w2.minus = allSuperObs$w2 - allSuperObs$w2.sigma
 w2.plus = allSuperObs$w2 + allSuperObs$w2.sigma
 quartz("NEOWISE binned")
-plot.title <- "NEOWISE Data binned"
+
+if(plot.w1) {
+	band.string <- "W1 & W2"
+}else {
+	band.string <- "W2"
+}
+
+plot.title <- paste("NEOWISE",band.string,"Data",bin.width,"day bins")
 
 errbar(x=allSuperObs$mjd,
 		y=allSuperObs$w2, 
 		yplus=w2.plus,yminus=w2.minus,
 		ylim=y.limits,xlim=t.limits,
-		xlab= "MJD",ylab="W2 magnitude",
+		xlab= "MJD",ylab="magnitude",
 		main=plot.title,
 		col=w2.col,errbar.col=bar.col,pch=20)
 		
+# put in a dashed line where the ALLWISE value is		
+lines(x=neowise.data$mjd,y=neowise.data$w2mpro_allwise,lty="dashed",col=w2.col)
+
+if(plot.w1) {
+	points(x=allSuperObs$mjd,
+			y=allSuperObs$w1,col=w1.col,pch=20)
+	lines(x=neowise.data$mjd,y=neowise.data$w1mpro_allwise,lty="dotted",col=w2.col)
+}
+
+# add title string to top
+title(plot.title)
+grid(col="black")
+
+quartz("W1 - W2")
+plot(x=allSuperObs$mjd,
+		y=allSuperObs$w1 - allSuperObs$w2, 
+		xlim=t.limits,
+		xlab= "MJD",ylab="magnitude difference",
+		main="W1 - W2",
+		col="darkred",pch=20)
 		
+
 grid(col="black")
