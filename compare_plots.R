abb2a2baa72bc6e7fcc8c3ed9eae17dfc034e33b
Paul Carr <paul.david.carr@gmail.com>
2017-12-19 11:47:27 -0500

    now plotting points and spline predictions

--- compare_plots.R
+++ compare_plots.R
@@ -1,15 +1,13 @@
 # R.bins
-# R.spline
 # V.bins
-# V.spline
 # B.bins
-# B.spline
-band.bias <- c(0.0,0.0,-0.39,-0.91)
+band.bias <- c(0.0,0.0,-0.380,-0.9023)
 plot.title <- "R, V and B bands superimposed"
 plot.pch = 20
+plot.cex <- 0.5
 x.pretty <- 10 # days
 JD.left <- 2457900
-JD.right <- 2458100
+JD.right <- 2458110
 tmin = floor(JD.left/x.pretty)*x.pretty
 plot.xlim <- c(0,ceil(JD.right/x.pretty)*x.pretty - tmin)
 x.label <- paste("days since",tmin)
@@ -34,20 +32,36 @@ plot(plot.days,plot.mags,
 	main = plot.title,
 	sub = "AAVSO data",
 	xlab = x.label,ylab=y.label,
-	type="p",col="red",pch=plot.pch,cex=0.5)
+	type="p",col="red",pch=plot.pch,cex= plot.cex)
 
+plot.pred <- R.bins$bin.predict + band.bias[2]
+lines(plot.days, plot.pred,
+	col="red",
+	lwd=1)
+	
 # plot V
 plot.mags = V.bins$Magnitude + band.bias[3]
 plot.days <- V.bins$JD - tmin
 
 points(plot.days,plot.mags,col="green",
-		type="p",pch=plot.pch,cex=0.5)
+		type="p",pch=plot.pch,cex= plot.cex)
+plot.pred <- V.bins$bin.predict + band.bias[3]
+lines(plot.days, plot.pred,
+	col="green",
+	lwd=1)
+
 
 #plot B
 plot.mags = B.bins$Magnitude + band.bias[4]
 plot.days <- B.bins$JD - tmin
 
 points(plot.days,plot.mags,col="blue",
-		type="p",pch=plot.pch,cex=0.5)
+		type="p",pch=plot.pch,cex= plot.cex)
+		
+plot.pred <- B.bins$bin.predict + band.bias[4]
+lines(plot.days, plot.pred,
+	col="blue",
+	lwd=1)
+
 
 grid(col="black")
