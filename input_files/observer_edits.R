f898acce63f805ef76ba35478908b35170996057
Paul Carr <paul.david.carr@gmail.com>
2018-03-08 13:19:31 -0500

    refinement of ASASSN bias

--- input_files/observer_edits.R
+++ input_files/observer_edits.R
@@ -61,4 +61,4 @@ biasObserver <- rbind(biasObserver, list(obsCode="OAR",band="B",bias=-0.0125),
                                     list("BMAK","B",-0.01),
                                     list("LWHA","B",0.0155),
                                     list("HBB","V",0.007),
-                                    list("ASASSN","V",0.04))
+                                    list("ASASSN","V",0.039))
