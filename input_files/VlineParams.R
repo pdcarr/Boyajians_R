8fcb0320f9068625c4bb4d38f829511f3b19fe64
Paul Carr <paul.david.carr@gmail.com>
2018-03-20 12:34:08 -0400

    tentative mask for 18Mar18 dip

--- input_files/VlineParams.R
+++ input_files/VlineParams.R
@@ -1,7 +1,7 @@
 ########## option to draw a vertical date line
 drawDateLine <-  TRUE
 #jdLine <- c(2457892.0,2457917.5)
-jdLine <- c(2457890, 2457915,2457968,2457994)
+jdLine <- c(2457890, 2457915,2457968,2457994,2458194)
 jdLineColor <- "red"
 #jdLineText <- c("Elsie","Celeste")
-jdLineText <- c("Elsie","Celeste","Skara Brae","Angkor")
+jdLineText <- c("Elsie","Celeste","Skara Brae","Angkor","Ozymandias")
--- input_files/dip_mask.R
+++ input_files/dip_mask.R
@@ -1,7 +1,8 @@
 dip.mask <- data.frame(dipname="Elsie",JD.begins = 2457890,JD.ends=2457896,stringsAsFactors=FALSE)
 dip.mask <- rbind(dip.mask,c("Celeste",2457915,2457927),
 						   c("Skara Brae",2457968,2457984),
-						   c("Angkor",2457994,2458011))
+						   c("Angkor",2457994,2458011),
+						   c("Ozymandias",2458194,2458197))
 
 # adding mask of FWAIN, as observed by Bruce Gary.
 #dip.mask <-rbind(dip.mask,c("FWAIN",2458051,2458057))
