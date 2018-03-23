7f989b3238db4c5d34b17ef6a8461a8916b54a84
Paul Carr <paul.david.carr@gmail.com>
2018-03-23 09:08:39 -0400

    updates to dip mask

--- input_files/dip_mask.R
+++ input_files/dip_mask.R
@@ -2,10 +2,10 @@ dip.mask <- data.frame(dipname="Elsie",JD.begins = 2457890,JD.ends=2457896,strin
 dip.mask <- rbind(dip.mask,c("Celeste",2457915,2457927),
 						   c("Skara Brae",2457968,2457984),
 						   c("Angkor",2457994,2458011),
-						   c("Ozymandias",2458194.05,2458199.5))
+						   c("Ozymandias",2458191.9,2458200))
 
 # adding mask of FWAIN, as observed by Bruce Gary.
 #dip.mask <-rbind(dip.mask,c("FWAIN",2458051,2458057))
 
 # primarily observed by Bruce Gary, the "December Surprise"
-#dip.mask <- rbind(dip.mask,c("December_Surprise",2458096,2458097))
+dip.mask <- rbind(dip.mask,c("December_Surprise",2458094,2458097))
