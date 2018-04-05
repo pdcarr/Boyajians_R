446068ba84c0526deb87cc5a7d2899ff84898348
Paul Carr <paul.david.carr@gmail.com>
2018-04-05 12:37:52 -0400

    Caral-Supe

--- input_files/VlineParams.R
+++ input_files/VlineParams.R
@@ -4,4 +4,4 @@ drawDateLine <-  TRUE
 jdLine <- c(2457890, 2457915,2457968,2457994,2458094.0,2458191.9)
 jdLineColor <- "red"
 #jdLineText <- c("Elsie","Celeste")
-jdLineText <- c("Elsie","Celeste","Skara Brae","Angkor","December","Evangeline")
+jdLineText <- c("Elsie","Celeste","Skara Brae","Angkor","December","Caral-Supe")
