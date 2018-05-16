64d3c4c2684114c7d1a3feac6fb837d3c9fc3266
Paul Carr <paul.david.carr@gmail.com>
2018-05-16 09:22:16 -0400

    new crude script to plot SG over gprime data

---
+++ SG_plot_AAVSO.R
@@ -0,0 +1,14 @@
+rm("sgb")
+if(exists('binCurve') & exists('tmin')) {
+	sgb <- binCurve$Band == "SG"
+	if(sum(sgb) <= 0) { 
+		cat("\nno SG data in binCurve\n")		
+		break
+	}
+	
+	SO.time <- binCurve$JD[sgb] - 2400000.5 - tmin
+	points(x=SO.time,
+		y=binCurve$Magnitude[sgb],
+		col="aquamarine3",
+		pch=1,cex=2.0)
+}
