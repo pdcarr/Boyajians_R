9b5383d9490f19571e5633714a264aae1b943b3e
Paul Carr <paul.david.carr@gmail.com>
2018-05-18 13:03:58 -0400

    improvement to SG plotting

--- SG_plot_AAVSO.R
+++ SG_plot_AAVSO.R
@@ -2,13 +2,13 @@ rm("sgb")
 if(exists('binCurve') & exists('tmin')) {
 	sgb <- binCurve$Band == "SG"
 	if(sum(sgb) <= 0) { 
-		cat("\nno SG data in binCurve\n")		
-		break
+		cat("\nno SG data in binCurve\n")
+	} else {		
+		
+		SO.time <- binCurve$JD[sgb] - 2400000.5 - tmin
+		points(x=SO.time,
+			y=binCurve$Magnitude[sgb],
+			col="aquamarine3",
+			pch=1,cex=2.0)
 	}
-	
-	SO.time <- binCurve$JD[sgb] - 2400000.5 - tmin
-	points(x=SO.time,
-		y=binCurve$Magnitude[sgb],
-		col="aquamarine3",
-		pch=1,cex=2.0)
 }
