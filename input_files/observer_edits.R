44885950327098a573f5dbc6bdbd0fff99591d9c
Paul Carr <paul.david.carr@gmail.com>
2018-08-07 17:00:42 -0400

    VMT I band bias

--- input_files/observer_edits.R
+++ input_files/observer_edits.R
@@ -65,4 +65,5 @@ biasObserver <- rbind(biasObserver, list(obsCode="OAR",band="B",bias=-0.0125),
                                     list("HBB","V",0.031),
                                     list("HBB","B",0.048),
                                     list("ASASSN","V",0.041),
-                                    list("VMT","V",-0.0275))
+                                    list("VMT","V",-0.0275),
+                                    list("VMT","V",0.03925))
