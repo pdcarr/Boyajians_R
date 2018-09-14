68142f45dd49d7481d144d9d3bf197bd4791cbcf
Paul Carr <paul.david.carr@gmail.com>
2018-09-14 16:31:45 -0400

    added SDB to airmass replacement

--- input_files/missing_airmass.R
+++ input_files/missing_airmass.R
@@ -10,6 +10,7 @@ Coventry <- c(52.4,-1.52) # approximate location of KTHC
 Ponferrada <- c(42.55,-6.598) # approximate location of PJVA
 Cotopaxi <- c(38.38,-105.69) # approximate location of HJW
 Minneapolis <- c(44.9778,-93.265)
+Auburn <- c(41.367,-85.06) # approximate location for SDB
 tabbysLoc <- c("+44d 27m 24.61s","20h 06m 15.457s") # declination and right ascension of the star.
-missingAirmass <- c("JM","PXR","JSJA","MATA","WROC","FJQ","PALE","KTHC","PJVA","HJW","DRZ")	# observer code
-missingAMLocs = rbind(lasCruces,Leominster,Warrington,Erd,Haan,LosAngeles,Potomac,Coventry,Ponferrada,Cotopaxi,Minneapolis)
+missingAirmass <- c("JM","PXR","JSJA","MATA","WROC","FJQ","PALE","KTHC","PJVA","HJW","DRZ","SDB")	# observer code
+missingAMLocs = rbind(lasCruces,Leominster,Warrington,Erd,Haan,LosAngeles,Potomac,Coventry,Ponferrada,Cotopaxi,Minneapolis,Auburn)
