8b24708f6807c1edf8044db94ec0b7019982de54
Paul Carr <paul.david.carr@gmail.com>
2018-09-17 17:18:24 -0400

    AAVSO update 17Sep18

--- input_files/aavso_bin_input_parameters.R
+++ input_files/aavso_bin_input_parameters.R
@@ -3,12 +3,12 @@
 llightcurve_name <- "data/aavso_latest_data.csv"
 # the name of the asassn file
 asassn.csv.file <- "data/ASASSN_latest.csv"
-merge.asassn <- TRUE # merge asasn data into light curve
+merge.asassn <- TRUE # merge asasn data into V light curve
 asassn.code <- "ASASSN"
 ######### filters #########################
-maxairmass <- 3.0 # air mass values above this will be filtered out, as well as missing air masses. Set >= 100 to turn this off
-maxuncertainty <- 0.02 # maximum AAVSO uncertainty estimate
-maxBinUncertainty <- 0.01 # worst standard deviation to accept for a binned set of observations
+maxairmass <- 2.2 # air mass values above this will be filtered out, as well as missing air masses. Set >= 100 to turn this off
+maxuncertainty <- 0.1 # maximum AAVSO uncertainty estimate
+maxBinUncertainty <- 0.02 # worst standard deviation to accept for a binned set of observations
 wildsd <- 100.0 # worst number of standard deviations from mean allowed
 ##########################################
 earliestJD = 2457290 # only data on or after this JD will be used
@@ -20,9 +20,10 @@ startPlot <- earliestJD
 #startPlot <- 2457880
 #startPlot <- 2457580
 #startPlot <- 2457990
-startPlot <- 2458150
+#startPlot <- 2458150
 #startPlot <- 2458000
 #startPlot <- 2457620
+startPlot <- 2458200
 #stop.plot <- 2457680
 #stop.plot <- 2458020
 add.predict <- 0 # additional prediction days to add to plot
@@ -31,28 +32,30 @@ plotRelTimes <- TRUE
 includeExclude <- TRUE # TRUE if your list of observer codes is to to be included, FALSE if excluded or not used
 ExclCodes <- "None"
 ExclCodes <- c("LDJ","DUBF","PXR","DKS","OJJ","SDB","VBPA","OAS","MJB","MATA",
-				"WROC","MAND","VBPA","NOT","PALE","GKA","AMID","SGEA","ELYA","GCJ","LBG","HJW","OAR","ASASSN") # V & B
+				"WROC","MAND","VBPA","NOT","PALE","GKA","AMID","SGEA","ELYA","GCJ","LBG","HJW","OAR","ASASSN","MMAO","JM","VMT","HBB","EEY","MNIC") # V & B
 #ExclCodes <- c("LDJ","BMAH")
 #ExclCodes <- c("LDJ")
 #ExclCodes <- c("ASASSN")
-#ExclCodes <- c("DUBF")
-#ExclCodes <- c("SWIA","CPP","DRZ","LPAC","HDHA") 	# B band exclusions
-#ExclCodes <- c("DUBF","GKA","BPAD","LPB","SJAR","LBG","LDJ","LWHA") # R ensemble
-#ExclCodes <- c("OAR","OJJ","GKA","MJB","SJAR","LWHA","LBG","LPB","LDJ","CMP","JM") # I ensemble
-#ExclCodes <- c("DUBF","MJB","LDJ","GKA","ELYA","HJW","JSJA","VBPA","DKS","OAR") # B ensemble
+ExclCodes <- c("DUBF","GKA","BPAD","LPB","SJAR","LBG","LDJ","LWHA","JM") # R ensemble
+#ExclCodes <- c("OAR","OJJ","GKA","MJB","SJAR","LWHA","LBG","LPB","LDJ","CMP","JM","VMT","SDB") # I ensemble
+#ExclCodes <- c("DUBF","MJB","LDJ","GKA","ELYA","HJW","JSJA","VBPA","DKS","OAR","HBB","JM") # B ensemble
 plotMee <- NA # do not highlight any particular observer code
-#plotMee <- "HBB"
+#plotMee <- "SDB"
+#plotMee <- "MMAO"
+#plotMee <- "MNIC"
 meeColor <- "darkviolet"
 weightless <- NA
-#weightless <- c("HBB") # observers to plot, but not use in fit.
-#weightless <- c("ASASSN","JSJA") # observers to plot, but not use in fit.
+weightless <- c("MNIC")
+#weightless <- c("EEY") # observers to plot, but not use in fit.
+#weightless <- c("HBB")
+#weightless <- c("SDB") # observers to plot, but not use in fit.
 ########
 allBands <- data.frame(bandinQ=c("I","R","V","B","SG"),plotColor=c("darkviolet","red","green","blue","aquamarine2"), stringsAsFactors=FALSE)
 allBands <- data.frame(bandinQ=c("V"),plotColor=c("darkgreen"), stringsAsFactors=FALSE)
 #allBands <- data.frame(bandinQ=c("V","B"),plotColor=c("green","blue"), stringsAsFactors=FALSE)
 #allBands <- data.frame(bandinQ=c("B"),plotColor=c("blue"), stringsAsFactors=FALSE)
-allBands <- data.frame(bandinQ=c("I"),plotColor=c("darkviolet"), stringsAsFactors=FALSE)
-#allBands <- data.frame(bandinQ=c("R"),plotColor=c("red"), stringsAsFactors=FALSE)
+allBands <- data.frame(bandinQ=c("R"),plotColor=c("red"), stringsAsFactors=FALSE)
+#allBands <- data.frame(bandinQ=c("I"),plotColor=c("darkviolet"), stringsAsFactors=FALSE)#allBands <- data.frame(bandinQ=c("R"),plotColor=c("red"), stringsAsFactors=FALSE)
 #allBands <- data.frame(bandinQ=c("SG"),plotColor=c("aquamarine2"),stringsAsFactors=FALSE)
 #allBands <- data.frame(bandinQ=c("I","R"),plotColor=c("darkviolet","red"), stringsAsFactors=FALSE)
 #allBands <- data.frame(bandinQ=c("I","R","B"),plotColor=c("darkviolet","red","blue"), stringsAsFactors=FALSE)
@@ -63,7 +66,7 @@ deltaJD <- 1.0 # bin width in days
 ########################
 plotExcluded <- FALSE # set to TRUE to plot the points in the lightcurve not used in the fit.
 plotQuadratic <- FALSE # set to TRUE to plot a quadratic fit
-generateTS <- FALSE # set to TRUE to creat a time series from the data
+generateTS <- FALSE # set to TRUE to create a time series from the data
 tsBinWidth <- 5.0 # time series bin width in days. Important if generateTS is TRUE
 smoothTS <- TRUE # set to TRUE to smooth the times series
 tsSmoothOrder <- 6 # the order for the moving average to smooth the time series
