6b196a7a405202cb7c1a021f5590b4c1f53a8539
Paul Carr <paul.david.carr@gmail.com>
2019-11-05 12:46:43 -0500

    adding ASASSN camera selection

--- plot_aavso_binned.R
+++ plot_aavso_binned.R
@@ -47,10 +47,12 @@ lightcurve <- read.csv(file=llightcurve_name,header=TRUE,check.names=TRUE,na.str
 totalRec = length(lightcurve$JD)
 cat("\n\nTotal records read from file: ",totalRec,"\n\n")
 
-if ((merge.asassn) & (sum(allBands$bandinQ %in% asassn.bands) > 0)){
+if ((merge.asassn) & (sum(allBands$bandinQ %in% asassn.bands) > 0) & sum(asassn.cameras %in% asassn_data$Camera) > 0){
 	cat("\nmerging in ASASSN Data\n")
+  cat("\nASAS-SN cameras",asassn.cameras,"\n")
 	asassn_data <- read.csv(asassn.csv.file,header=TRUE,stringsAsFactors=FALSE)
-	lightcurve <- asassn.merge(lightcurve,asassn_data,asassn.code,g.to.V= convert.asassn,star.BminusV= our.BminusV,V.bias= converted.V.bias) # function merges asassn data into lightcurve
+	ok.camera <- !is.na(match(asassn_data$Camera,asassn.cameras))
+	lightcurve <- asassn.merge(lightcurve,asassn_data[ok.camera,],asassn.code,g.to.V= convert.asassn,star.BminusV= our.BminusV,V.bias= converted.V.bias) # function merges asassn data into lightcurve
 } else {
   cat("\nNot merging ASAS-SN data - no relevant bands\n")
 }
